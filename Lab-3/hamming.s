1cd801e35b320556d6fb3baa5467d8b0a3fe9f63
isisney1 <isisney1@umbc.edu>
2026-09-17 19:23:16 +0000

    Refactor hamming.s to improve instruction consistency and add Hamming distance calculation

--- Lab-3/hamming.s
+++ Lab-3/hamming.s
@@ -7,56 +7,80 @@ prompt_2_len = . - prompt_2
 .section .bss
 .lcomm string_1, 256                # reserve 256 bytes for string 1 (255 characters)
 .lcomm string_2, 256                # reserve 256 bytes for string 2 (255 characters)
+.lcomm hamming_distance, 4          # reserve 4 bytes for hamming distance
 
 .section .text
 .global _start
 _start:
     # Write prompt 1 to stdout
-    mov $1, %rax                    # write
-    mov $1, %rdi                    # stdout
-    mov $prompt_1, %rsi             # buf
-    mov $prompt_1_len, %rdx         # len
+    movq $1, %rax                   # write
+    movq $1, %rdi                   # stdout
+    movq $prompt_1, %rsi            # buf
+    movq $prompt_1_len, %rdx        # len
     syscall
 
     # Read string 1 from stdin
-    mov $0, %rax                    # read
-    mov $0, %rdi                    # stdin
-    mov $string_1, %rsi             # buf
-    mov $256, %rdx                  # len
+    movq $0, %rax                   # read
+    movq $0, %rdi                   # stdin
+    movq $string_1, %rsi            # buf
+    movq $256, %rdx                 # len
     syscall
 
-    mov %eax, %ebx                  # set EBX to string 1 size
+    movl %eax, %ebx                 # set EBX to string 1 size
 
     # Write prompt 2 to stdout
-    mov $1, %rax                    # write
-    mov $1, %rdi                    # stdout
-    mov $prompt_2, %rsi             # buf
-    mov $prompt_2_len, %rdx         # len
+    movq $1, %rax                   # write
+    movq $1, %rdi                   # stdout
+    movq $prompt_2, %rsi            # buf
+    movq $prompt_2_len, %rdx        # len
     syscall
 
     # Read input 2 from stdin
-    mov $0, %rax                    # read
-    mov $0, %rdi                    # stdin
-    mov $string_2, %rsi             # buf
-    mov $256, %rdx                  # len
+    movq $0, %rax                   # read
+    movq $0, %rdi                   # stdin
+    movq $string_2, %rsi            # buf
+    movq $256, %rdx                 # len
     syscall
 
     # Check if the second string is smaller
-    cmp %eax, %ebx                  # compare sizes of string 1 and 2
+    cmpl %eax, %ebx                 # compare sizes of string 1 and 2
     jle compare                     # jump to compare loop if string 1 is smaller
-    mov %eax, %ebx                  # set EBX to string 2 size
+    movl %eax, %ebx                 # set EBX to string 2 size
 
     compare:
-        mov string_1, %rsi
-        mov string_2, %rdi
+        movq $string_1, %rsi
+        movq $string_2, %rdi
+        movl $0, %ecx
 
-        
     character_loop:
-        dec %ebx                    # EBX = EBX - 1
+        movb (%rsi), %ah
+        movb (%rdi), %al
+
+        xorb %ah, %al
+        
+        count_ones:
+            testb $1, %al
+            jz file_through
+            incl %ecx
+
+        file_through:
+            shrb $1, %al
+            jnz count_ones
+
+        incq %rsi
+        incq %rdi
+        decl %ebx                   # EBX = EBX - 1
         jnz character_loop          # continue looping if EBX != 0
 
+    movl %ecx, hamming_distance
+    
+    movq $1, %rax                   # write
+    movq $1, %rdi                   # stdout
+    movq $hamming_distance, %rsi    # buf
+    movq $4, %rdx                   # len
+    syscall
 
     done:
-        mov $60, %rax               # exit
-        mov $0,  %rdi               # status
+        movq $60, %rax              # exit
+        movq $0,  %rdi              # status
         syscall
